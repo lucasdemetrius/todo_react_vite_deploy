5abb24f28059743650c9959b0d406bc872db86ba
Lucas Jesus <lucas.jesus@flowti.com.br>
2024-06-12 10:45:14 -0300

    Finalizando projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,88 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import "./App.css";
+
+import Todo from "./components/Todo"
+import TodoForm from './components/TodoForm';
+import Search from './components/Search';
+import Filter from './components/Filter';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "Criar funcionalidade X no sistema",
+      category: "Trabalho",
+      isCompleted: false,
+    },
+  ]);
+
+  const addTodo = (text, category) => {
+
+    const newTodos = [...todos, {
+      id: Math.floor(Math.random() * 10000),
+      text,
+      category,
+      isCompleted: false,
+    },
+    ];
+
+    setTodos(newTodos);
+  }
+
+  const removeTodo = (id) => {
+
+    const newTodos = [...todos]
+    const filteredTodos = newTodos.filter(todo =>
+      todo.id !== id ? todo : null
+    );
+    setTodos(filteredTodos);
+
+  }
+
+  const completeTodo = (id) => {
+
+    const newTodos = [...todos]
+    newTodos.map((todo) =>
+      todo.id === id ? (todo.isCompleted = !todo.isCompleted) : todo
+    );
+    setTodos(newTodos);
+  }
+
+  const [search, setSearch] = useState("");
+
+  const [filter, setFilter] = useState("All");
+  const [sort, setSort] = useState("Asc");
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app">
+      <h1>Lista de tarefas</h1>
+      <Search search={search} setSearch={setSearch} />
+      <Filter filter={filter} setFilter={setFilter} setSort={setSort} />
+      <div className="todo-list">
+        {todos
+          .filter((todo) => 
+            filter === "All" 
+              ? true 
+              : filter === "Completed" 
+              ? todo.isCompleted 
+              : !todo.isCompleted
+          )
+          .filter((todo) =>
+            todo.text.toLowerCase().includes(search.toLowerCase()))
+          .sort((a, b) => 
+            sort === "Asc" 
+              ? a.text.localeCompare(b.text) 
+              : b.text.localeCompare(a.text)
+          )
+          .map((todo) => (
+            <Todo key={todo.id} todo={todo} removeTodo={removeTodo} completeTodo={completeTodo} />
+          ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <TodoForm addTodo={addTodo} />
+    </div>
+  );
 }
 
 export default App
---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+
+// eslint-disable-next-line react/prop-types
+const TodoForm = ({ addTodo }) => {
+
+    const [value, setValue] = useState("");
+    const [category, setCategory] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+        if (!value || !category) return;
+
+        //Adicionando tarefa
+        addTodo(value, category);
+
+        //Limpando os campos
+        setValue("");
+        setCategory("");
+        
+    }
+
+  return (
+    <div className='todo-form'>
+      <h2>Criar tarefa:</h2>
+      <form onSubmit={handleSubmit}>
+        <input  
+          type="text" 
+          placeholder="Digite o título"
+          value={value}
+          onChange={(e) => setValue(e.target.value)} 
+        />
+        <select value={category} onChange={(e) => setCategory(e.target.value)}>
+            <option value="">Selecione uma categoria</option>
+            <option value="Trabalho">Trabalho</option>
+            <option value="Pessoal">Pessoal</option>
+            <option value="Estudos">Estudos</option>
+        </select>
+        <button type='submit'> Criar tarefa </button>
+      </form>
+    </div>
+  )
+}
+
+export default TodoForm
